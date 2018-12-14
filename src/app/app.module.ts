29ade58f39f1cfe7869e0e4269aefe923d2dd0a1
thulasiram <thulasiram.ponnam@in.verizon.com>
2018-12-14 09:01:40 +0530

    ebooks commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BooksComponent } from './books/books.component';
+import { BookDetailComponent } from './books/book-detail/book-detail.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
+    BooksComponent,
+    BookDetailComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
